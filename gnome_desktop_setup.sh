192a68adbb689b8971273e9750cd26bc79aef7bf
游晨烯 <60802368+chencyu@users.noreply.github.com>
2020-11-30 21:40:21 +0800

    [BugFix] Can't recognize path sometimes.

--- gnome_desktop_setup.sh
+++ gnome_desktop_setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-BASEDIR=$(dirname "$0")
-bash $BASEDIR/ubuntu-wsl2-systemd-script.sh
+BASEDIR=$(realpath "$BASH_SOURCE")
+BASEDIR=$(dirname "$BASEDIR")
+bash "$BASEDIR/ubuntu-wsl2-systemd-script.sh"
 sudo apt update
 sudo apt install ubuntu-desktop
 sudo chown -R $USER:$USER ~/.cache
